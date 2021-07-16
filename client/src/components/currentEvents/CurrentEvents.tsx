cc436610255b59d2ea4d744a39cffc62168ab035
sudeshlak <swelikotuwa@gmail.com>
2021-07-17 00:12:29 +0530

    styles of header links

--- client/src/components/currentEvents/CurrentEvents.tsx
+++ client/src/components/currentEvents/CurrentEvents.tsx
@@ -9,10 +9,27 @@ import {IoNotificationsCircle} from "react-icons/all";
 
 const CurrentEvents = () => {
   const [toggled, setToggled] = useState(false);
-  const [events,setEvents]=useState([{eventName:'carier fair 2021'},{eventName:'carier fair 2021'},{eventName:'carier fair 2020'},{eventName:'carier fair 2019'}]);
+  const [events,setEvents]=useState([
+    {id:1,eventName:'carier fair 2021'},
+    {id:2,eventName:'carier fair 2021'},
+    {id:3,eventName:'carier fair 2020'},
+    {id:4,eventName:'carier fair 2019'}
+  ]);
+
   const handleToggleSidebar = (value: boolean) => {
     setToggled(value);
   };
+
+  /*const renderEvents = () =>{
+    return(
+      events.map((event)=>{
+        <Event eventName={event.eventName}
+               id={event.id}
+               key={event.id}
+        />
+      })
+    );
+  }*/
   return (
     <React.Fragment>
       <CssBaseline/>
@@ -28,11 +45,17 @@ const CurrentEvents = () => {
               </div>
             </Col>
             <Col xs={6} className='text-info text-right'>
-              <IoNotificationsCircle size='2.5em'/>
+              <IoNotificationsCircle size='2.7em'/>
             </Col>
           </Row>
+
           <Row>
+            <Col xs={12}>
+              <Row>{events[0].eventName}</Row>
+
+            </Col>
           </Row>
+
         </main>
       </Container>
       <Footer title="Footer" description="Something here to give the footer a purpose!"/>
---
+++ client/src/components/currentEvents/events/Event.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Event = () => {
+  return(
+    <React.Fragment>
+    </React.Fragment>
+  );
+}
+
+export default Event;
