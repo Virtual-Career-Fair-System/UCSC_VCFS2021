c1c81cff563fc755e975666ee470f2b10a535ab6
Nimantha815 <nimanthaaravinda19970815@gmail.com>
2021-07-18 14:09:37 +0530

    sign in page

--- client/src/components/login/Login.tsx
+++ client/src/components/login/Login.tsx
@@ -1,20 +1,161 @@
-import React from "react";
-import CssBaseline from "@material-ui/core/CssBaseline";
+
 import Header from "../header/Header";
-import {Container} from "react-bootstrap";
+
 import Footer from "../footer/Footer";
 
-const Login = () => {
-  return(
+
+import React, { useState } from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Axios from 'axios';
+import ProfilePage from "../profile/ProfilePage";
+// import { response } from 'express';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link color="inherit" href="https://material-ui.com/">
+        Your Website
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
+
+export default function SignIn() {
+  const classes = useStyles();
+
+  const [email,setEmail]= useState("");
+  const [password,setPassword]= useState("");
+
+  const [loginStatus, setLoginStatus] = useState("");
+
+  const login = () => {
+    // console.log(fname);
+    Axios.post('http://localhost:3001/login',{
+        
+        email: email, 
+        password: password,
+    }).then((response)=>{
+        if(response.data.message){
+          setLoginStatus(response.data.message)
+        }else{
+          setLoginStatus(response.data[0].email)
+        }
+    });
+};
+
+  return (
     <React.Fragment>
-      <CssBaseline/>
       <Header title="Career Fair UCSC"/>
-      <Container fluid={true}>
-        Login
-      </Container>
-      <Footer title="Footer" description="Something here to give the footer a purpose!"/>
+    <Container component="main" maxWidth="xs">
+      
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <form className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+            onChange={(event)=>{
+              setEmail(event.target.value);
+          }}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(event)=>{
+              setPassword(event.target.value);
+          }}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            onClick={login}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href="#" variant="body2">
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href="#" variant="body2">
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+      <h1>{loginStatus}</h1>
+      <Box mt={8}>
+        <Copyright />
+      </Box>
+    </Container>
+    <ProfilePage/>
+    <Footer title="Footer" description="Something here to give the footer a purpose!"/>
     </React.Fragment>
   );
 }
 
-export default Login;
---
+++ client/src/components/profile/ProfilePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from '../header/Header'
+export default function ProfilePage() {
+    return (
+        <div>
+            {/* <Header/> */}
+        </div>
+    )
+}
+
+
