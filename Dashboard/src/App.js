1e43fa2f1894af69a8721c0339cdd873db32b29a
Pasindu Sooriyabandara <56555775+PasinduChanusha@users.noreply.github.com>
2021-09-19 08:39:52 +0530

    Add files via upload

---
+++ Dashboard/src/App.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import "./App.css";
+import Main from "./components/main/Main";
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+
+const App = () => {
+  const [sidebarOpen, setsidebarOpen] = useState(false);
+  const openSidebar = () => {
+    setsidebarOpen(true);
+  };
+  const closeSidebar = () => {
+    setsidebarOpen(false);
+  };
+  return (
+    <div className="container">
+      <Navbar sidebarOpen={sidebarOpen} openSidebar={openSidebar} />
+      <Main />
+      <Sidebar sidebarOpen={sidebarOpen} closeSidebar={closeSidebar} />
+    </div>
+  );
+};
+
+export default App;
