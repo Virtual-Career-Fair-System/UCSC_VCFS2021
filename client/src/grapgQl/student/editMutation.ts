308f5192b0d8ec69285f8355d931159186882b22
sudeshlak <swelikotuwa@gmail.com>
2021-09-22 21:39:47 +0530

    address error

--- client/src/grapgQl/student/editMutation.ts
+++ client/src/grapgQl/student/editMutation.ts
@@ -31,7 +31,7 @@ export const UPDATE_STUDENT = gql`
         skills: $skills,
         contactNumber:$contactNumber,
         linkedIn:$linkedIn,
-        address:$adress,
+        address:$address,
         profilePicture:$profilePicture,
         coverPhoto:$coverPhoto
         ){
